48c4c600e51902235a5d117e4f58b28cf6a10c15
nwalsh2102 <nwalsh0221@gmail.com>
2025-06-05 17:20:23 -0400

    auth baseURL --> prod url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://qr-gen-d2svapupq-nicks-projects-6009e7bc.vercel.app/",
 });
